9be139926d7a9ea0aad43c995e04c80d88f98fce
Paulo Silva <ph.montefusco@gmail.com>
2021-08-14 23:50:32 -0300

    projeto inicial

---
+++ QueueConsumer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace rpcserver
+{
+    public static class QueueConsumer
+    {
+
+        public static void Consume(IModel channel)
+        {
+            channel.QueueDeclare(queue: "fila_helo", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                    channel.BasicAck(ea.DeliveryTag,false);
+                }
+                catch (Exception ex)
+                {
+                    channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+            channel.BasicConsume(queue: "fila_helo", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Introdução 
+Exemplo de projeto utilizando RabbitMQ para Consumer
+
+
+# Ambiente de desenvolvimento
+Esse projeto poder utilizar o vs code 
+As dependencias dele estão baseado no NET 5.0
+devem baixar os seguintes arquivos
+- SDK 5
+- ASP NET CORE RUNTIME 5 (HOSTING BUDLE)
+
+
